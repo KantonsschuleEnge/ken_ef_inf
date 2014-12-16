5fc2026fd04aead7586260f92fbbdd144ddf4b88
SinianSung <gregor.luedi@ken.ch>
2014-12-16 13:46:35 +0100

    testprogramm for the course

---
+++ assembler/simpleTest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+
+	int a = 0;
+
+
+	if(a == 24){
+		printf("\n\nGeheimnis gelüftet!\n\n");
+	}
+	else{
+		printf("\n\nfalscher Wert!\n\n");
+	}
+	return 0;
+}
